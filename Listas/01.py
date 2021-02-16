ec421e94f884cc20b25230b430da37d016ed94fe
Pedro Suarez <pedrosuarezmula@gmail.com>
2021-02-16 12:55:45 +0100

    listas

---
+++ Listas/01.py
@@ -0,0 +1,28 @@
+"""
+Ejercicio 01
+
+Descargar una copia del archivo "romeo.txt" desde http://www.pythonlearn.com/code3/romeo.txt.
+ Escribir un programa para abrir el fichero y leerlo línea a línea. Por cada línea separar las palabras en una
+lista de palabras usando la función split. Las palabras no deben estar repetidas en la lista.
+Cuando la lista esté completa, visualizar el resultado en orden alfabético.
+
+"""
+
+try:
+    fhandle = open("romeo.txt")
+except IOError:
+    print("El fichero no existe.")
+    exit()
+
+words_list = list()
+
+
+for line in fhandle:
+    for word in line.split():
+        if word not in words_list:
+            words_list.append(word)
+words_list.sort()
+
+for word in words_list:
+    print(word, sep=' ', end='\n')
+
---
+++ Listas/03.py
@@ -0,0 +1,28 @@
+"""
+Ejercicio 03:
+
+Reescribir el programa que solicita al usuario una lista de número y muestra el valor máximo y mínimo de entre ellos y
+acaba con la palabra "done". Hacerlo ahora almacenando los números en una lista y hacer uso de las funciones
+max() y min().
+
+"""
+
+numbers_list = list()
+done = False
+
+while not done:
+    reading = True
+    while reading:
+        string = input('Introduzca un número ("fin" para terminar):')
+        if string == "fin":
+            done = True
+            break
+        try:
+            number = float(string)
+            reading = False
+        except IOError:
+            print("Entrada incorrecta, pruebe de nuevo.")
+            continue
+        numbers_list.append(number)
+print("El valor máximo es %.2f y el mínimo %.2f" % (max(numbers_list),
+      min(numbers_list)))
